8b1ec46b658866b0278ea4392dafe8e335e0b8dd
ATISHAY JAIN <atishayj2202@gmail.com>
2024-01-26 16:09:06 +0530

    Create User Table

--- migrations/V1__create_user_table.sql
+++ migrations/V1__create_user_table.sql
@@ -1,15 +1,15 @@
-CREATE TYPE auth_type AS ENUM ('google', 'github', 'facebook', 'apple');
+CREATE TYPE user_type AS ENUM ('employee', 'employer');
 
 CREATE TABLE user_accounts (
     id UUID PRIMARY KEY,
     created_at TIMESTAMPTZ DEFAULT now()::TIMESTAMPTZ,
     last_modified_at TIMESTAMPTZ DEFAULT now()::TIMESTAMPTZ,
-    email VARCHAR NOT NULL,
+    phone_no VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     firebase_user_id VARCHAR NOT NULL,
-    auth_type auth_type NOT NULL,
+    user_type user_type NOT NULL,
     UNIQUE(firebase_user_id),
-    UNIQUE(email),
+    UNIQUE(phone_no),
     INDEX idx_firebase_user_id (firebase_user_id),
-    INDEX idx_email (email)
+    INDEX idx_phone_no (phone_no)
 );
