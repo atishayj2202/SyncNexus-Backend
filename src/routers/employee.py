d7a3d2f4ec2d60252cfe89802db7e82c311bb891
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-01-26 20:56:13 +0530

    Update employee.py

--- src/routers/employee.py
+++ src/routers/employee.py
@@ -9,3 +9,4 @@ ENDPOINT_GET_TASK = "/{task_id}/get-task/"  # pending
 ENDPOINT_GET_JOB_DESCRIPTION = "/{task_id}/get-job-description/"  # pending
 ENDPOINT_COMPLETE_TASK = "/{task_id}/complete-task/"  # pending
 ENDPOINT_GET_EMPLOYER = "/{employer_id}/get-employer/"  # pending
+ENDPOINT_ADD_LOCATION = "/add-location/"  # pending
