81eec7a42c7e706abe35836ba48951ff72f04d9e
Tanmay0215 <tanmaygupta.0215@gmail.com>
2024-01-27 11:29:32 +0530

    def post till add end employee endpt

---
+++ src/responses/job.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from src.utils.enums import JobStatus
+
+class JobCreateRequest(BaseModel):
+    employer_id: UUID
+    employee_id: UUID
+    heading: str
+    description: str | None
+    last_date: str | None
+    location_lat: float
+    lacation_long: float
+    done: str | None
+    amount : int
+
+class JobResponse(BaseModel):
+    id: UUID
+    created_at: datetime
+    employer_id: UUID
+    employee_id: UUID
+    heading: str
+    description: str | None = None
+    last_date: str | None = None
+    status: JobStatus
