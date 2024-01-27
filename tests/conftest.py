81727a3298cc7dd43157cd09907b6928cb7cc886
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-01-27 11:29:44 +0530

    Testing Setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,64 @@ def firebase_client():
     return FirebaseClient()
 
 
+@pytest.fixture(scope="function")
+def make_random_email(cockroach_client):
+    def fn():
+        email = "abc" + str(random.randint(100, 10000)) + "@gmail.com"
+        user = cockroach_client.query(
+            User.get_by_field_unique,
+            field="phone_no",
+            match_value=email,
+        )
+        if user is not None:
+            return fn()
+        return email
+
+    return fn
+
 @pytest.fixture(scope="session")
 def app_test_client():
     return TestClient(app)
 
+
+@pytest.fixture(scope="session")
+def fast_api_auth_header_random_employee(firebase_client, make_random_email, cockroach_client) -> tuple[User, dict]:
+    email = make_random_email()
+    password = "omjALeay"
+    firebase_user = firebase_client.create_user(email, password)
+    user = User(
+                id=uuid4(),
+                type=UserType.employee,
+                email=email,
+                firebase_user_id=str(firebase_user.uid),
+            )
+    cockroach_client.query(
+        User.add,
+        items=[
+            user
+        ],
+    )
+    yield user, {"Authorization": f"Bearer {firebase_client.get_user_token(firebase_user.uid)}"}
+    firebase_client.delete_user(firebase_user.uid)
+
+
+
+@pytest.fixture(scope="session")
+def fast_api_auth_header_random_employer(firebase_client, make_random_email, cockroach_client) -> tuple[User, dict]:
+    email = make_random_email()
+    password = "omjALeay"
+    firebase_user = firebase_client.create_user(email, password)
+    user = User(
+                id=uuid4(),
+                type=UserType.employer,
+                email=email,
+                firebase_user_id=str(firebase_user.uid),
+            )
+    cockroach_client.query(
+        User.add,
+        items=[
+            user
+        ],
+    )
+    yield user, {"Authorization": f"Bearer {firebase_client.get_user_token(firebase_user.uid)}"}
+    firebase_client.delete_user(firebase_user.uid)
