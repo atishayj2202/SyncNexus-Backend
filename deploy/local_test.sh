6ed0a09a0c00e6abe0e29ec3e084f867a7b3b509
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-02-10 00:21:28 +0530

    Migrated from cockroach DB to Google Cloud SQL

--- deploy/local_test.sh
+++ deploy/local_test.sh
@@ -4,6 +4,9 @@ usage="Usage: sh deploy/local_test.sh [build|push]"
 export COCKROACH_CONTAINER_NAME=cockroachdb/cockroach:v21.2.7
 export COCKROACH_DB_LOCAL_URL="cockroachdb://root@localhost:26257/defaultdb?sslmode=disable"
 export COCKROACH_DATA_PATH="$PWD"/cockroach-data/
+export GOOGLE_CLOUD_SQL_CONNECTION="winged-octagon-413211:asia-south2:google-solution-challenge"
+export GOOGLE_CLOUD_SQL_CREDENTIALS_FILE="$PWD"/keys/winged-octagon-413211-ce95d7afb9ff.json
+
 
 function get_container() {
     docker ps --filter "ancestor=${1}" --format '{{.ID}}' | sed -n 1p
@@ -63,6 +66,13 @@ then
   poetry env use ~/.pyenv/versions/3.10.12/bin/python
   poetry install
   sh deploy/local_test.sh pull
+elif [[ $1 = "pull-auth-proxy" ]]
+then
+  curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.8.2/cloud-sql-proxy.darwin.arm64
+  chmod +x cloud-sql-proxy
+elif [[ $1 = "remote-start" ]]
+then
+  ./cloud-sql-proxy $GOOGLE_CLOUD_SQL_CONNECTION --credentials-file=$GOOGLE_CLOUD_SQL_CREDENTIALS_FILE --port=3308
 else
   echo "${usage}"
   exit 1
--- src/client/cockroach.py
+++ src/client/cockroach.py
@@ -1,20 +1,40 @@
 import os
 from typing import Any, Callable, Optional
 
-from sqlalchemy import create_engine
+import sqlalchemy
 from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy_cockroachdb import run_transaction
 
 
 class CockroachDBClient:
     ENV_URL = "COCKROACH_DB_URL"
 
     def __init__(self, url: Optional[str] = None):
-        self.url = url or os.environ[self.ENV_URL]
-        self.engine = create_engine(self.url, max_overflow=100, pool_size=10)
+        # self.url = url or os.environ[self.ENV_URL]
+        self.db_name = os.environ["DB_NAME"]
+        self.db_user = os.environ["DB_USER"]
+        self.db_pass = os.environ["DB_PASS"]
+        self.unix_socket_path = os.environ["UNIX_SOCKET_PATH"]
+        self.engine = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL.create(
+                drivername="postgresql+pg8000",
+                username=self.db_user,
+                password=self.db_pass,
+                database=self.db_name,
+                query={"unix_sock": f"{self.unix_socket_path}/.s.PGSQL.5432"},
+            ), max_overflow=100, pool_size=10)
 
     def get_session_maker(self):
         return sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
     def query(self, fn: Callable[[Session, ...], Any], **kwargs):
-        return run_transaction(self.get_session_maker(), lambda s: fn(s, **kwargs))
+        # Instead of using run_transaction, directly execute the provided function
+        session = self.get_session_maker()()
+        try:
+            result = fn(session, **kwargs)
+            session.commit()
+            return result
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
